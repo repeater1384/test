e7d0e1ba0739afa0b14702dfc17ccce609b0c9f7
repeater1384 <repeater1384@naver.com>
2020-07-19 19:02:36 +0900

    CLASS ADD

--- temp.py
+++ temp.py
@@ -1,4 +1,14 @@
+class Soldier:
+    def __init__(self, _생활관, _이름, _계급):
+        self.생활관 = _생활관
+        self.이름 = _이름
+        self.계급 = _계급
 
-with open('123.txt','w') as file:
-    for i in range(32,128):
-        file.write(chr(i)+'\n')
+    def get_string_info(self):
+        string_info = f'{self.계급} {self.이름}({self.생활관})'
+        return string_info
+
+name = '박일규'
+info = {}
+info[name] = Soldier('군2',name,'상병')
+print(info[name].get_string_info())
